c2b8f5560adaeb889b69338ffa82d46fe0248a72
manojlds <manojlds@gmail.com>
2014-01-27 02:47:58 +0530

    Triggering a checksum change

--- src/main/java/com/stacktoheap/maven_ci_example/App.java
+++ src/main/java/com/stacktoheap/maven_ci_example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello CI with TeamCity!" );
+        System.out.println( "Hello Maven CI with TeamCity!" );
     }
 }
